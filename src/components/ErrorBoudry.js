986f6fc553989c6a83dd57461c17d73c24425659
pedro kalva <pedro.kalva@gmail.com>
2023-03-13 23:15:48 -0300

    ajusta errorBoudry

--- src/components/ErrorBoudry.js
+++ src/components/ErrorBoudry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class ErrorBoudry extends component {
+class ErrorBoudry extends Component {
     constructor(props) {
         super(props);
         this.state = {
